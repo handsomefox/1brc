0e5e007ed548915e7f3a35b7eaf2e3e9810e9a7d
Eduard Diachenko <handsomefox.hf@gmail.com>
2025-12-04 12:50:42 +0200

    mmap

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"maps"
 	"math"
 	"os"
 	"slices"
 	"strconv"
-	"strings"
+	"syscall"
 )
 
 type Measurement struct {
@@ -24,52 +24,92 @@ func (m *Measurement) Merge(value float32) {
 }
 
 func NewMeasurement(value float32) *Measurement {
-	return &Measurement{Sum: value, Min: min(math.MaxFloat32, value), Max: max(-math.MaxFloat32, value), Total: 1}
-}
-
-func ParseLine(line string) (city string, value float32) {
-	split := strings.SplitN(line, ";", 2)
-	measureFloat, err := strconv.ParseFloat(split[1], 32)
-	if err != nil {
-		panic(err)
+	return &Measurement{
+		Sum:   value,
+		Min:   min(math.MaxFloat32, value),
+		Max:   max(-math.MaxFloat32, value),
+		Total: 1,
 	}
-	return split[0], float32(measureFloat)
 }
-
-const (
-	MB      = 1 << 20
-	BufSize = 128 * MB
-)
 
 func main() {
-	f, err := os.Open("measurements.txt")
+	file, err := os.Open("measurements.txt")
 	if err != nil {
 		panic(err)
 	}
-	sc := bufio.NewScanner(f)
-	sc.Buffer(make([]byte, 0, BufSize), BufSize)
-	data := make(map[string]*Measurement, 10_000)
 
-	for sc.Scan() {
-		line := sc.Text()
-		city, measurement := ParseLine(line)
-		if existing, ok := data[city]; ok {
+	var (
+		fileBytes      = mmap(file)
+		fileReadOffset = 0
+		measurements   = make(map[string]*Measurement, 10_000)
+		nlBytes        = []byte{'\n'}
+	)
+
+	for fileReadOffset < len(fileBytes) {
+		nl := bytes.IndexByte(fileBytes[fileReadOffset:], '\n')
+
+		var line []byte
+		if nl == -1 {
+			line = fileBytes[fileReadOffset:]
+			fileReadOffset = len(fileBytes)
+		} else {
+			line = fileBytes[fileReadOffset : fileReadOffset+nl]
+			fileReadOffset += nl + 1
+		}
+		if len(line) == 0 || bytes.Equal(line, nlBytes) {
+			continue
+		}
+
+		sep := bytes.IndexByte(line, ';')
+		cityBytes := line[:sep]
+		valBytes := line[sep+1:]
+		val, err := strconv.ParseFloat(string(valBytes), 32)
+		if err != nil {
+			panic(err)
+		}
+
+		city, measurement := string(cityBytes), float32(val)
+		if existing, ok := measurements[city]; ok {
 			existing.Merge(measurement)
 		} else {
-			data[city] = NewMeasurement(measurement)
+			measurements[city] = NewMeasurement(measurement)
 		}
 	}
 
-	keys := slices.Collect(maps.Keys(data))
+	keys := slices.Collect(maps.Keys(measurements))
 	slices.Sort(keys)
 
 	fmt.Print("{")
 	for i, key := range keys {
-		measure := data[key]
+		measure := measurements[key]
 		fmt.Printf("%s=%.1f/%.1f/%.1f", key, measure.Min, measure.Sum/float32(measure.Total), measure.Max)
 		if i != len(keys)-1 {
 			fmt.Print(" ")
 		}
 	}
 	fmt.Print("}\n")
+}
+
+func mmap(f *os.File) []byte {
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := syscall.Mmap(
+		int(f.Fd()),
+		0,
+		int(fi.Size()),
+		syscall.PROT_READ,
+		syscall.MAP_PRIVATE,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := syscall.Madvise(data, syscall.MADV_SEQUENTIAL); err != nil {
+		panic(err)
+	}
+
+	return data
 }
